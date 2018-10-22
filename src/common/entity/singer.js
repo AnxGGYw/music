a1312f2498a146a1563882f04d1d05098d817a95
cggcbb <449567533@qq.com>
2018-10-22 10:34:42 +0800

    抓取并整理歌手数据

---
+++ src/common/entity/singer.js
@@ -0,0 +1,7 @@
+export default class Singer {
+  constructor({ mid, name }) {
+    this.mid = mid
+    this.name = name
+    this.avatar = `https://y.gtimg.cn/music/photo_new/T001R150x150M000${this.mid}.jpg?max_age=2592000`
+  }
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,80 @@
 <template>
-  <div>
-   歌手页面
+  <div class="singer">
+    
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Singer from 'common/entity/singer'
+import { getSingerList } from 'api/singer'
 
+const HOT_NAME = '热'
+const HOT_SINGER_NUM = 10
+
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    async _getSingerList() {
+      let list = []
+      let res = await Promise.all([getSingerList(1), getSingerList(2)]) // 分页 200 条数据
+      res.forEach(item => {
+        list.push(...item.data.list)
+      })
+      this.singers = this._normalizeSingers(list)
+    },
+    _normalizeSingers(list) {
+      let map = {
+        'hot': {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((ele, index) => {
+        if (index < HOT_SINGER_NUM) {
+          map.hot.items.push(new Singer({
+            mid: ele.Fsinger_mid,
+            name: ele.Fsinger_name
+          }))
+        }
+        const fIndex = ele.Findex // 首字母
+        if (!map[fIndex]) {
+          map[fIndex] = {
+            title: fIndex,
+            items: []
+          }
+        }
+        map[fIndex].items.push(new Singer({
+          mid: ele.Fsinger_mid,
+          name: ele.Fsinger_name
+        }))
+      })
+      // js 遍历对象不能保证顺序, 这里需要转换成数组
+      let hotArr = []
+      let othersArr = []
+      for (let key in map) {
+        if (map.hasOwnProperty(key)) {
+          let value = map[key]
+          if (value.title.includes(HOT_NAME)) {
+            hotArr.push(value)
+          } else if (/[a-zA-Z]/.test(value.title)) {
+            othersArr.push(value)
+          }
+        }
+      }
+      othersArr.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hotArr.concat(othersArr)
+    }
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
