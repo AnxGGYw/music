81f55a5f58362eee2e06972c59758545406b73e1
cggcbb <449567533@qq.com>
2019-02-22 11:15:03 +0800

    update store

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import { playMode } from 'common/js/play.config.js'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false, // 播放状态(默认关闭)
+  fullScreen: false, // 全屏(默认不全屏)
+  playList: [], // 播放列表 (有顺序,单曲,随机播放)
+  sequenceList: [], // 顺序列表
+  mode: playMode.sequence, // 播放模式(默认顺序播放)
+  currentIndex: -1 // 当前播放歌曲index
 }
 
 export default state
